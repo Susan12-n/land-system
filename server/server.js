2ee743fb0a0e28065c01d1ef4da41d3ba78feef8
Susan12-n <ngesasuzan@gmail.com>
2025-07-23 14:58:33 +0300

    Use dynamic port for Render

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log(" MongoDB connected");
 
-    const PORT = process.env.PORT;
+    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
